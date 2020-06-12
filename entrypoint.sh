39f6e9aad2c3e979ac26d077ea8d340f8d9abf57
airuleguy <ale.iruleguy@gmail.com>
2020-06-11 22:44:46 -0300

    Failed miserably to correctly set bucket address.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-(aws s3 ls s3:${AWS_S3_BUCKET}${FILE}) && \
+(aws s3 ls s3://${AWS_S3_BUCKET}${FILE}) && \
      rm -rf ~/.aws && \
      echo "Ooops! Version already exists, aborting upload..." && \
      exit 1 \
-|| (aws s3 cp ${FILE} s3:${AWS_S3_BUCKET} --region ${AWS_REGION} $*) && rm -rf ~/.aws
+|| (aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} --region ${AWS_REGION} $*) && rm -rf ~/.aws
