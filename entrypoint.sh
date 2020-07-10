5e8938a2a2555dcd79300ef2bf23ee0d9d05f727
Daryoush <daryoush@gotab.io>
2020-07-09 21:08:08 -0400

    Remove hard-coded excludes and read from environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 sync  --exclude ".git*" --exclude "*.DS_Store" --exclude "utils/deprecated/*" --exclude "*.pyc" --exclude "cache*" --exclude ".vscode*" --exclude "*.spec" --exclude "*Dockerfile*" --exclude "utils/sample_receipts*" --exclude "build.sh" --exclude ".dockerignore" --exclude ".stage" ${SOURCE_PATH} s3://${AWS_S3_BUCKET}${DESTINATION_KEY} --region ${AWS_REGION}
+aws s3 sync ${EXCLUDES} ${INCLUDES} ${SOURCE_PATH} s3://${AWS_S3_BUCKET}${DESTINATION_KEY} --region ${AWS_REGION}
