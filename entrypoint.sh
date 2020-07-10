593b111c8448d591b4d190ae01f7308164a7c5d8
Daryoush Mansouri <daryoush@gotab.io>
2020-07-10 17:39:59 -0400

    Trying something new

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,8 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 echo "Excludes:  ${EXCLUDES}"
 echo "Includes:  ${INCLUDES}"
-aws s3 sync ${SOURCE_PATH} s3://${AWS_S3_BUCKET}${DESTINATION_KEY} --region ${AWS_REGION} ${INCLUDES} ${EXCLUDES} --debug
+
+tar -zcvf artifacts.tar.gz ${EXCLUDES} ${SOURCE_PATH}
+
+aws s3 mv artifacts.tar.gz s3://${AWS_S3_BUCKET}${DESTINATION_KEY} --region ${AWS_REGION} --debug
+#aws s3 sync ${SOURCE_PATH} s3://${AWS_S3_BUCKET}${DESTINATION_KEY} --region ${AWS_REGION} ${INCLUDES} ${EXCLUDES} --debug
